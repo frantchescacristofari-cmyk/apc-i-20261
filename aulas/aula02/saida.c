df6de7ad23fa064f91384f97076794e8deb5d66f
unknown <2612130031@ACADEMICO.IESB.BR>
2026-03-11 10:57:41 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    // 1. Entrada
+
+    // 2. Processamento 
+
+    // 3. Saida
+    printf("Um texto qualquer\n");
+    printf(""%i\n", 10");
+    printf("%f\n", 10.3454");
+
+    return 0;
+}
