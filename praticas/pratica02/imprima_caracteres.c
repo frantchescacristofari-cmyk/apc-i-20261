f45a80d518d1a28842712ee6155b3ddd3835a4b6
frantchesca <frantchesca.cristofari@gmail.com>
2026-04-08 09:40:49 -0300

    feat: conclui pratica02. Fecha ID#3

---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("%c\n", 'A');
+    printf("Isso aqui eh uma string\n");
+    return 0;
+}
---
+++ praticas/pratica02/imprima_formatado.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("==========================\n");
+    printf("         NOTA LEGAL\n");
+    printf("==========================\n");
+    printf("Produto             Qtd           Valor Unit\n");
+    printf("Camiseta            002            39.99\n");
+    printf("Calca               001            89.90\n");
+    printf("Meia Social         003            19.90\n");
+    printf("=================================================");
+    printf("Total             229.85\n");
+    return 0;
+}
