6821557c14dd1afbe9bd24d7f2bc22dae0682b7b
frantchesca <frantchesca.cristofari@gmail.com>
2026-05-06 10:15:30 -0300

    feat:nao clonclui pratica 06

---
+++ praticas/pratica06/par_impar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+
+    printf("Entre com um numero: ");
+    scanf("%i, numero");
+
+    int par = numero % 2 = 0;
+    int impar numero % 2 != 0;
+
+    if (numero_par) {
+        printf("%i eh par", numero par);
+    }
+    if (numero impar) {
+        printf("%i eh impar", numero impar);
+    }
+    return 0;
+}
